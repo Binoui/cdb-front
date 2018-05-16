17849a7f8f36dbbff30bccb3edf56326fb449ae1
bhoux <bhoux@excilys.com>
2018-05-16 17:14:40 +0200

    init company component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CompanyComponent } from './company/company.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/company/company.model.ts
@@ -0,0 +1,4 @@
+export class company {
+    id: number;
+    name : string;
+}
