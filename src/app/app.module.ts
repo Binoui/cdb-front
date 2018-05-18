726a87122b1a22f830869cd3a002fa387e28a8b0
Flotte <excilys@localhost.localdomain>
2018-05-18 15:41:19 +0200

    [feat] Details - Html done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './/app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './/app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { CompanyComponent } from './company/company.component';
-import { CompaniesComponent } from './company/companies/companies.component';
+import {AppComponent} from './app.component';
+import {CompanyComponent} from './company/company.component';
+import {CompaniesComponent} from './company/companies/companies.component';
 import {CompanyDetailComponent} from './company/company-detail/company-detail.component';
-import { ComputerComponent } from './computer/computer.component';
+import {ComputerComponent} from './computer/computer.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CustomMaterialModule} from './custom-material/custom-material.module';
 
--- src/app/company/company.module.ts
+++ src/app/company/company.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CompanyDetailComponent } from './company-detail/company-detail.component';
 import {CustomMaterialModule} from '../custom-material/custom-material.module';
+import {MatListModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CustomMaterialModule,
+    MatListModule,
     CommonModule
   ],
   declarations: [CompanyDetailComponent]
