f7c200ff45b2342b738a8a92bcba9db1a7aaa1bb
Flotte <excilys@localhost.localdomain>
2018-05-22 13:59:14 +0200

    [Merge] Merge corrected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,15 @@
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './/app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './/app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
 
-<<<<<<< HEAD
-import { AppComponent } from './app.component';
-import { CompanyComponent } from './company/company.component';
-import { CompaniesComponent } from './company/companies/companies.component';
-import { CompanyDetailComponent } from './company/company-detail/company-detail.component';
-import { ComputerComponent } from './computer/computer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
 import {AppComponent} from './app.component';
 import {CompanyComponent} from './company/company.component';
 import {CompaniesComponent} from './company/companies/companies.component';
 import {CompanyDetailComponent} from './company/company-detail/company-detail.component';
 import {ComputerComponent} from './computer/computer.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {CustomMaterialModule} from './custom-material/custom-material.module';
 import {ComputerDetailComponent} from './computer/computer-detail/computer-detail.component';
 import {ComputersComponent} from './computer/computers/computers.component';
->>>>>>> feature/computer-detail
 
 @NgModule({
   declarations: [
